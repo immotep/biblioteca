940e2fe3cfabd193a4b8718e25bb60cdbf3d188d
NavaL Andrian <immotep@gmail.com>
2012-09-28 09:29:04 +0200

    42. added BookTest.testBookShouldNotBeReservedWhenCreated() to make sense of testReserveABook().

--- test/com/twu29/biblioteca/BookTest.java
+++ test/com/twu29/biblioteca/BookTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class BookTest {
+
+    @Test
+    public void testBookShouldNotBeReservedWhenCreated() throws Exception {
+        Book book = new Book("id");
+
+        assertEquals(false, book.isReserved());
+    }
+
     @Test
     public void testReserveABook() throws Exception {
          Book book = new Book("someID");
--- test/com/twu29/biblioteca/MenuItemTest.java
+++ test/com/twu29/biblioteca/MenuItemTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class MenuItemTest {
 
     @Test
-    public void testCommandIsExecuted() throws Exception {
+    public void testDefaultMessageWhenCommandIsExecuted() throws Exception {
         String response = "interesting";
         MenuItem menuItem = new MenuItem("something the user can do, appearing on the menu", response);
 
