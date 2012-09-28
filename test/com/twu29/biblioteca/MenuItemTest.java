cf7f8bc7dc82db817ebef5193018e1a63f69b892
NavaL Andrian <immotep@gmail.com>
2012-09-28 09:19:48 +0200

    41. removed test for getDescription because it's a getter with no restriction and renamed testGetResponseMessage() to testDefaultMessageWhenCommandIsExecuted()

--- test/com/twu29/biblioteca/MenuItemTest.java
+++ test/com/twu29/biblioteca/MenuItemTest.java
@@ -7,15 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class MenuItemTest {
 
     @Test
-    public void testGetDescription() throws Exception {
-        String description = "something the user can do, appearing on the menu";
-        MenuItem menuItem = new MenuItem(description, "some execute");
-
-        assertEquals(description, menuItem.getDescription());
-    }
-
-    @Test
-    public void testGetResponseMessage() throws Exception {
+    public void testCommandIsExecuted() throws Exception {
         String response = "interesting";
         MenuItem menuItem = new MenuItem("something the user can do, appearing on the menu", response);
 
